4f0c043cdf226bd720b467462faad2e99fb38966
jnoh213 <jacknelson365@byui.edu>
2024-04-28 11:28:09 -0600

    prep 1 and 2 work

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string f_name= Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string l_name= Console.ReadLine();
+        Console.WriteLine($"Your name is {l_name}, {f_name} {l_name}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,76 @@
 using System;
+using System.Formats.Asn1;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // GRADING SCALE
+        // A >= 90
+        // B >= 80
+        // C >= 70
+        // D >= 60
+        // F < 60
+        Console.Write("What is your Grade? ");
+        string stringGrade= Console.ReadLine();
+        int grade=int.Parse(stringGrade);
+        string letter;
+        string denominator="";
+        if(grade>=90){
+            letter="A";
+            if(grade%10<=3){
+                denominator="-";
+            }
+            else{
+                denominator="";
+            }
+        }
+        else if(grade>=80){
+            letter="B";
+            if(grade%10>=7){
+                denominator="+";
+            }
+            else if(grade%10<=3){
+                denominator="-";
+            }
+            else{
+                denominator="";
+            }
+        }
+        else if(grade>=70){
+            letter="C";
+            if(grade%10>=7){
+                denominator="+";
+            }
+            else if(grade%10<=3){
+                denominator="-";
+            }
+            else{
+                denominator="";
+            }
+        }
+        else if (grade>=60){
+            letter="D";
+            if(grade%10>=7){
+                denominator="+";
+            }
+            else if(grade%10<=3){
+                denominator="-";
+            }
+            else{
+                denominator="";
+            }
+        }
+        else{
+            letter="F";
+        }
+        if(letter=="A" || letter=="B" ||letter=="C" || letter=="D"){
+            
+            Console.WriteLine($"You passed with a {letter}{denominator}.");
+        }
+        else{
+            Console.WriteLine("Try Again, you got this!");
+        }
     }
 }
