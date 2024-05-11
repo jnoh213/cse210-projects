a304a993da0bb466691271a21260d5058e517fbd
jnoh213 <jacknelson365@byui.edu>
2024-05-10 19:01:17 -0600

    W03 Prepare

---
+++ sandbox/Sandbox/Course.cs
@@ -0,0 +1,14 @@
+
+using System.ComponentModel.DataAnnotations;
+
+class Course
+{
+    public string _courseCode;
+    public string _courseName;
+    public int _creditHours;
+    public string _color;
+    
+    public void Display(){
+        Console.WriteLine();
+    }
+}
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Course course1 =new Course();
+        course1._courseCode="CSE 210";
+        course1._courseName="Prog with classes";
+        course1._creditHours=2;
+        course1._color="green";
+        course1.Display();        
     }
 }
